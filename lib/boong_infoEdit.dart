11b78ba723ce79761007e22383ce52bd8d49aa15
Ludence <ludence8@o.cnu.ac.kr>
2022-05-06 00:35:17 +0900

    가게정보 수정 화면 테스트 및 일부 구현

--- lib/boong_infoEdit.dart
+++ lib/boong_infoEdit.dart
@@ -1,4 +1,8 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:io';
 
 void main() => runApp(MyApp());
 
@@ -21,6 +25,8 @@ class infoEdit extends StatefulWidget {
 class infoEditState extends State<infoEdit> {
   String name = "";
   List<String> entries = <String>["HelloWorld!"];
+  List<String> pictures = <String>['a.jpg','b.jpg','c.jpg','d.jpg']; // 임시 사진 업로드 체크용 사진. 이후 사진 수정 예정
+  List<String> menu = <String>[""];
   int a =0;
 
   final TextEditingController controller = TextEditingController();
@@ -30,7 +36,7 @@ class infoEditState extends State<infoEdit> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("가게 정보 수정"),
+        title: Text("어서오세요!"),
         backgroundColor: Colors.black26,
       ),
       body: Container(
@@ -117,17 +123,97 @@ class infoEditState extends State<infoEdit> {
                             TextStyle(fontSize: 20, color: Colors.white)),
                         backgroundColor:
                         MaterialStateProperty.all(Colors.grey)),
-                    child: Text("+"),)
+                    child: Text("+"),),
+                  Container(margin: EdgeInsets.all(8.0),),
+
+                  Text("가게 사진 (" +'$a' +"/99)",
+                    style: TextStyle(
+                        fontSize: 20, fontWeight: FontWeight.bold),),
+
+                  Container(
+                    padding: EdgeInsets.fromLTRB(100, 0, 100, 0),
+                    child: Row(
+                      children: [Flexible(child: ListView.builder(
+                        shrinkWrap: true,
+                        itemCount: a,
+                        itemBuilder: (BuildContext context, int index) {
+                          return Container(
+                            color: Colors.black26,
+                            child: Center(child: Image(image: AssetImage('assets/images/${pictures[index]}'),), // 사진 업로드 체크용. 이후 갤러리 열어서 사진 넘기는 쪽으로 수정 예정
+                            ),
+                          );
+                        },
+                      ),)],
+                    ),
+                  ),
+
+                  ElevatedButton(
+                    onPressed: () {
+                      setState(() {
+                        if(a >= pictures.length) { //임시.
+                        }
+                        else {
+                          a++;
+                        }
+                      });
+                    },
+                    style: ButtonStyle(
+                        textStyle: MaterialStateProperty.all(
+                            TextStyle(fontSize: 20, color: Colors.white)),
+                        backgroundColor:
+                        MaterialStateProperty.all(Colors.grey)),
+                    child: Text("+"),),
+
+                  Text("메뉴명/가격",
+                    style: TextStyle(
+                        fontSize: 20, fontWeight: FontWeight.bold),),
+                  ListView.separated(
+                    shrinkWrap: true,
+                    itemCount: menu.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      return Container(
+                          height : 50,
+                          color: Colors.black26,
+                          child: Center(child: Text('${menu[index]}'),)
+                      );
+                    }, separatorBuilder: (BuildContext context, int index) => const Divider(),
+                  ),
+                  ElevatedButton( // 이후 화면 구성 후 처리 예정
+                    onPressed: () {},
+                    style: ButtonStyle(
+                        textStyle: MaterialStateProperty.all(
+                            TextStyle(fontSize: 20, color: Colors.white)),
+                        backgroundColor:
+                        MaterialStateProperty.all(Colors.grey)),
+                    child: Text("+"),),
+
 
                 ],
               )),
-
+              Container(margin: EdgeInsets.all(10.0),),
+              Container(
+                margin: EdgeInsets.fromLTRB(0, 70, 0, 0),
+                padding: EdgeInsets.fromLTRB(0, 0, 0, 0),
+                color: Colors.black26,
+                child : Center(
+                  child: ElevatedButton( //미입력된 부분 존재시 넘어가지 못하게 하는 부분 처리 x
+                    onPressed: () {},
+                    style: ButtonStyle(
+                        textStyle: MaterialStateProperty.all(
+                            TextStyle(fontSize: 20, color: Colors.white)),
+                        backgroundColor:
+                        MaterialStateProperty.all(Colors.grey)),
+                    child: Text("입력 완료", style: TextStyle(fontSize: 30, color: Colors.black, fontWeight: FontWeight.bold),),),
+                ),
+              )
 
 
             ],
           ),
         ),
       ),
+
     );
   }
 }
+
